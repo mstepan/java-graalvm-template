06acd0e476a398523eaeaf60eb04b9d32f22295c
Maksym Stepanenko <stepanenkomaksi@gmail.com>
2025-04-04 12:22:05 +0300

    Create multiple native threads

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+JVM_OPTIONS="-Xms1G -Xmx1G -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1"
+
+java $JVM_OPTIONS --enable-preview -jar target/java23-graalvm-template-0.0.1-SNAPSHOT.jar
--- src/main/java/com/github/mstepan/template/AppMain.java
+++ src/main/java/com/github/mstepan/template/AppMain.java
@@ -1,9 +1,30 @@
 package com.github.mstepan.template;
 
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
+
 public class AppMain {
 
     public static void main(String[] args) {
 
-        System.out.println("Main done...");
+        final AtomicInteger threadsCount = new AtomicInteger(0);
+
+        try {
+            while (true) {
+                Thread.ofPlatform()
+                        .start(
+                                () -> {
+                                    int counter = threadsCount.incrementAndGet();
+
+                                    if (counter % 10_000 == 0) {
+                                        System.out.printf("Counter: %d%n", threadsCount.get());
+                                    }
+
+                                    LockSupport.park();
+                                });
+            }
+        } catch (OutOfMemoryError ex) {
+            System.out.printf("Created threads count: %d%n", threadsCount.get());
+        }
     }
 }
