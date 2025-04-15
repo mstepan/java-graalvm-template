fafa3cf9a8d5d6c6f2ccd85325434db01d03e13b
Maksym Stepanenko <stepanenkomaksi@gmail.com>
2025-04-15 15:12:29 +0300

    Throughput calcualtor fo rdifferent pools added

--- src/main/java/com/github/mstepan/template/AppMain.java
+++ src/main/java/com/github/mstepan/template/AppMain.java
@@ -1,1 +1,57 @@
-package com.github.mstepan.template;import java.util.concurrent.atomic.AtomicInteger;import java.util.concurrent.locks.LockSupport;public class AppMain {    public static void main(String[] args) {        final AtomicInteger threadsCount = new AtomicInteger(0);        try {            while (true) {                Thread.ofPlatform()                        .start(                                () -> {                                    int counter = threadsCount.incrementAndGet();                                    if (counter % 10_000 == 0) {                                        System.out.printf("Counter: %d%n", threadsCount.get());                                    }                                    LockSupport.park();                                });            }        } catch (OutOfMemoryError ex) {            System.out.printf("Created threads count: %d%n", threadsCount.get());        }    }}
+package com.github.mstepan.template;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class AppMain {
+
+    public static void main(String[] args) throws Exception {
+        final int tasksCount = 10_000;
+
+        Runnable ioTask =
+                () -> {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(500L);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                };
+
+        measureThroughput(
+                "virtual", Executors.newVirtualThreadPerTaskExecutor(), tasksCount, ioTask);
+//        measureThroughput("pool-100", Executors.newFixedThreadPool(100), tasksCount, ioTask);
+//        measureThroughput("pool-250", Executors.newFixedThreadPool(250), tasksCount, ioTask);
+        measureThroughput("pool-1000", Executors.newFixedThreadPool(1000), tasksCount, ioTask);
+
+        System.out.println("Main done...");
+    }
+
+    private static void measureThroughput(
+            String type, ExecutorService pool, int tasksCount, Runnable task) {
+        Instant start = Instant.now();
+
+        AtomicLong completedTasks = new AtomicLong();
+
+        try (pool) {
+            for (int i = 0; i < tasksCount; i++) {
+                pool.execute(
+                        () -> {
+                            task.run();
+                            completedTasks.incrementAndGet();
+                        });
+            }
+        }
+
+        Instant end = Instant.now();
+
+        long durationInMs = Duration.between(start, end).toMillis();
+
+        System.out.printf(
+                "[%s], time: %d ms, throughput: %.1f RPS%n",
+                type, durationInMs, (((double) completedTasks.get() * 1000.0) / durationInMs));
+    }
+}
